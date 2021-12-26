9ddd4131b73aa71efbb238eed2a4eb17a108cc18
Tavares Neto <tavares.agtn@gmail.com>
2021-12-26 20:56:49 -0300

    Incluído função e tela de autenticação, agenda multiusuário

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from core.models import Evento
 
-# Register your models here.
-
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'data_evento', 'data_criacao')
     list_filter = ('usuario', 'data_evento')
